2fd59059a9cac6b2a5202cea45a9021b45c75f77
luorigong <luorigong@wacai.com>
2018-05-14 20:31:25 +0800

    feat: 二手车网站爬虫

---
+++ usedCarData/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+
+class UsedCarDataItem(Item):
+    # define the fields for your item here like:
+    name = Field()
+    brand = Field()
+    runway = Field()
+    time = Field()
+    city = Field()
+    money = Field()
